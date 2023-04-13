13fbde3abe55bc8e566ff919d61351ad216124b2
Muhammad Ali Yousaf <m.aliyousaf1133@gmail.com>
2023-04-13 14:10:18 -0400

    added color in node js

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
 const app = require("./app");
 const dotenv = require("dotenv");
 const mongoConnection = require("./config/db");
+const colors = require("colors");
 
 // uncatchError...... like(console.log(youtube))
 process.on("uncatchError", (error) => {
-  console.log(`Shutdown the server due to unCatchError ${error.message}`);
+  console.log(`Shutdown the server due to unCatchError ${error.message}`.bgRed);
   process.exit(1);
 });
 
@@ -14,11 +15,11 @@ dotenv.config({
 
 mongoConnection();
 const server = app.listen(process.env.PORT, () => {
-  console.log(`server is starting at http://localhost:${process.env.PORT}`);
+  console.log(`server start http://localhost:${process.env.PORT}`.bgYellow);
 });
 
 // unhandle rejection...... (MongoDb Connection String)
 process.on("unhandledRejection", (error) => {
-  console.log(`Shutdown the server due to unHandlError ${error.message}`);
+  console.log(`Shutdown the server due to unHandlError ${error.message}`.bgRed);
   server.close(() => process.exit(1));
 });
